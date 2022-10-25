c4f678fb5aa46cc545fdb53d00208537e151809b
bee-thedev <beenyshsaeed@gmail.com>
2022-10-25 22:18:03 +0500

    Index.html for Class36 updated

--- Class36-material/js/main.js
+++ Class36-material/js/main.js
@@ -1,16 +1,28 @@
-document.querySelector('#clickMe').addEventListener('click', coinFlip)
+// document.querySelector('#clickMe').addEventListener('click', coinFlip)
 
-function coinFlip(){
-  let face = ''
-  let randomNumber = Math.random() * 2;
+// function coinFlip(){
+//   let face = ''
+//   let randomNumber = Math.random() * 2;
 
-  if(randomNumber > 1){
-    face = "Heads"
-    document.querySelector('#coinImg').innerHTML = '<img id="" src="./img/heads.png" alt="">'
-  }else{
-    face = "Tails"
-    document.querySelector('#coinImg').innerHTML = '<img id="" src="./img/tails.png" alt="">'
-  }
-  document.querySelector("#coinFace").innerHTML = face
-  console.log(randomNumber);
-} 
+//   if(randomNumber > 1){
+//     face = "Heads"
+//     document.querySelector('#coinImg').innerHTML = '<img id="" src="./img/heads.png" alt="">'
+//   }else{
+//     face = "Tails"
+//     document.querySelector('#coinImg').innerHTML = '<img id="" src="./img/tails.png" alt="">'
+//   }
+//   document.querySelector("#coinFace").innerHTML = face
+//   console.log(randomNumber);
+// } 
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
